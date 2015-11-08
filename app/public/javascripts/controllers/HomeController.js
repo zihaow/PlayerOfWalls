65957b0765318f37a4643cbf3fb12a8e0be0bdc3
zihaow <zihao.wu1993@gmail.com>
2015-11-07 21:45:24 -0400

    added text animation

--- app/public/javascripts/controllers/HomeController.js
+++ app/public/javascripts/controllers/HomeController.js
@@ -1,4 +1,19 @@
 app.controller('HomeController',function($scope, $rootScope, $route, $http){
+	
+	// INIT VARIABLES
 	$scope.showBackground = true;
+	$scope.showSlideShow = true;
+	$scope.skipPhoto = true;
+	
+	
+	$(document).ready(function() {
+   $('.text-container').css("animation-delay", "40s");
+	});
+	
 
+	$scope.skip = function() {
+		$scope.showSlideShow = false;
+		$scope.skipPhoto = false;
+		$('.text-container').css("animation-delay", "2s");
+	};
 });
