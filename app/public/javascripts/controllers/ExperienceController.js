9163c58ed710673c2cd7f625bf0cba22f599a0fd
zihaow <zihao.wu1993@gmail.com>
2015-11-08 16:02:45 -0400

    experience page 1 done

--- app/public/javascripts/controllers/ExperienceController.js
+++ app/public/javascripts/controllers/ExperienceController.js
@@ -1,3 +1,55 @@
-app.controller('ExperienceController',function($scope, $rootScope, $route, $http){
+app.controller('ExperienceController',function($scope, $rootScope, $route, $http, $timeout){
 	$scope.showBackground = false;
+	$scope.showConfidence = false;
+	$scope.showTeamwork = false;
+	$scope.showTimeManagement = false;
+	$scope.showScroll = false;
+
+	// overflow auto 
+	$(document).ready(function() {
+   $('.content').css("overflow", "auto");
+	});
+
+	$scope.showConfident = function() {
+		$('.confidence-section').removeClass("slideOutRight animated");
+		$('.confidence-section').addClass("slideInLeft animated");
+		$scope.showConfidence = true;
+	};
+	$scope.hideConfidence = function() {
+		$('.confidence-section').removeClass("slideInLeft animated");
+		$('.confidence-section').addClass("slideOutRight animated");
+	};
+
+	$scope.showTeamworks = function() {
+		$('.teamwork-section').removeClass("flipOutY animated");
+		$('.teamwork-section').addClass("rotateInUpRight animated");
+		$scope.showTeamwork = true;
+	};
+	$scope.hideTeamwork = function() {
+		$('.teamwork-section').removeClass("rotateInUpRight animated");
+		$('.teamwork-section').addClass("flipOutY animated");
+	};
+
+	$scope.showTimeMgmt= function() {
+		$('.timeMgmt-section').removeClass("zoomOutRight animated");
+		$('.timeMgmt-section').addClass("zoomInRight animated");
+		$scope.showTimeManagement = true;
+	};
+	$scope.hideTimeMgmt = function() {
+		$('.timeMgmt-section').removeClass("zoomInRight animated");
+		$('.timeMgmt-section').addClass("zoomOutRight animated");
+	};
+
+	// show scroll button after 5s.
+	$timeout(function() {
+    $scope.showScroll = true;
+  }, 5000);
+
+  // hide scroll button after 10s.
+	$timeout(function() {
+		$('.scroll').addClass("rotateOutUpRight animated");
+		$('.scroll-v').addClass("rotateOutUpRight animated");
+  }, 9000);
+
+
 });
