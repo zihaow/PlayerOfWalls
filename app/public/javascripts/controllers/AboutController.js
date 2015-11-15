8fa03904188f4c4ee93fc8204a158c6bf85c4220
zihaow <zihao.wu1993@gmail.com>
2015-11-15 14:45:05 -0400

    about page added hint

--- app/public/javascripts/controllers/AboutController.js
+++ app/public/javascripts/controllers/AboutController.js
@@ -1,10 +1,19 @@
 app.controller('AboutController',function($window, $scope, $rootScope, $route, $http){
 	$scope.showBackground = false;
+	$scope.showPage1S = true;
+	$scope.showPage2S = true;
+	$scope.showPage3S = true;
 	
 
-	$(document).ready(function() {
-		var windowWidth = $(window).width();
-		
-		
-	});
+	$scope.hidePage1 = function() {
+		$scope.showPage1S = false;
+	};
+
+	$scope.hidePage2 = function() {
+		$scope.showPage2S = false;
+	};
+
+	$scope.hidePage3 = function() {
+		$scope.showPage3S = false;
+	};
 });
